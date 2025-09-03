ff239882ad331332564cdb5377d2f55227c8e569
FahmiNaghmouchi <92573156+FahmiNaghmouchi@users.noreply.github.com>
2025-09-03 12:58:07 +0100

    up

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    
+
     const handleSubmit = async(e) => {
         e.preventDefault();
         
